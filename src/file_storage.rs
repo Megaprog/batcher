49105b264d778384e811aac749639aaad87d3078
Tomas Shestakov <shestakovte@gmail.com>
2020-01-30 23:57:44 +0300

    try to write into file

--- src/file_storage.rs
+++ src/file_storage.rs
@@ -1,40 +1,48 @@
 use std::collections::VecDeque;
 use std::sync::Arc;
-use crate::batch_storage::BinaryBatch;
-use crate::waiter::Lock;
+use crate::batch_storage::{BinaryBatch, BatchStorage, BatchFactory};
+use crate::waiter::{Lock, Waiter};
 use std::path::{Path, PathBuf};
-use std::fs::File;
-use std::{io, fs};
+use std::fs::{File, OpenOptions};
+use std::{io, fs, fmt};
 use log::*;
-use std::ops::Deref;
-use std::io::{Error, ErrorKind, BufRead};
-use std::str::FromStr;
-use std::ffi::OsStr;
+use std::io::{Error, ErrorKind, BufRead, Write, Seek, SeekFrom};
+use std::fmt::{Display, Formatter, Debug};
 
 macro_rules! batch_file {
     () => ( "batchFile" )
 }
+macro_rules! file_id_pattern {
+    () => ( "{:011}" )
+}
 
 static BATCH_FILE: &str = batch_file!();
 static BATCH_FILE_NAME_PREFIX: &str = concat!(batch_file!(), "-");
-static DIGITAL_FORMAT: &str = "{:011}";
-static LAST_BATCH_ID_FILE_NAME: &str = "lastBatchId";
+static LAST_BATCH_ID_FILE_NAME: &str = "nextBatchId";
 
 const DEFAULT_MAX_BYTES_IN_FILES: u64 = std::u64::MAX;
 
 pub struct FileStorageSharedState {
     pub(crate) file_ids: VecDeque<i64>,
     occupied_bytes: u64,
-    last_batch_id: i64,
+    next_batch_id: i64,
+    batch_id_file: File,
     stopped: bool,
 }
 
 #[derive(Clone)]
 pub struct FileStorage {
     path: PathBuf,
-    batch_id_file: PathBuf,
     pub max_bytes: u64,
     pub(crate) shared_state: Arc<Lock<FileStorageSharedState>>
+}
+
+impl Debug for FileStorage {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("FileStorage")
+            .field("path", &self.path)
+            .finish()
+    }
 }
 
 impl FileStorage {
@@ -49,7 +57,6 @@ impl FileStorage {
             return Err(Error::new(ErrorKind::NotFound, format!("The path {:?} is not a directory", path)))
         }
 
-
         let mut ids_and_sizes = fs::read_dir(&path)?
             .filter(|dir_entry_res| dir_entry_res.as_ref()
                 .map(|dir_entry| dir_entry.path().is_file()).unwrap_or(true) )
@@ -63,19 +70,19 @@ impl FileStorage {
             .map(|result| result
                 .and_then(|name_meta| FileStorage::batch_id(&name_meta.0).map(|id| (id, name_meta.1))))
             .collect::<Result<Vec<_>, io::Error>>()?;
-        let batch_id_file = path.join(LAST_BATCH_ID_FILE_NAME);
 
+        let batch_id_file = OpenOptions::new().read(true).write(true).create(true)
+            .open(path.join(LAST_BATCH_ID_FILE_NAME))?;
 
-        let last_batch_id = {
-            let file = fs::OpenOptions::new().read(true).create(true).open(&batch_id_file)?;
-            let mut lines = io::BufReader::new(file).lines();
+        let next_batch_id = {
+            let mut lines = io::BufReader::new(batch_id_file).lines();
             lines.next().map(|result| result.and_then(|s| s.parse::<i64>()
                 .map_err(|e| Error::new(ErrorKind::InvalidData,
                                         format!("Can't parse last batch id value '{}' from file {}", s, e)))))
                 .unwrap_or(Ok(ids_and_sizes.last().map(|id_size| id_size.0).unwrap_or(0)))?
         };
 
-        debug!("Init lastBatchId with {}", last_batch_id);
+        debug!("Init next batch id with {}", next_batch_id);
         debug!("Initialized {} batches.", ids_and_sizes.len());
 
         ids_and_sizes.sort();
@@ -83,12 +90,12 @@ impl FileStorage {
 
         Ok(FileStorage {
             path,
-            batch_id_file,
             max_bytes,
             shared_state: Arc::new(Lock::new(FileStorageSharedState {
                 file_ids,
                 occupied_bytes: sizes.into_iter().sum(),
-                last_batch_id,
+                next_batch_id,
+                batch_id_file,
                 stopped: false,
             })),
         })
@@ -99,5 +106,64 @@ impl FileStorage {
             .map_err(|e|
                 Error::new(ErrorKind::InvalidData, format!("Can't parse file name '{}' got {}", batch_file_name, e)))
     }
+
+    fn is_capacity_exceeded(&self, batch: &BinaryBatch, waiter: &Waiter<FileStorageSharedState>) -> bool {
+        waiter.occupied_bytes + batch.bytes.len() as u64 > self.max_bytes
+    }
+
+    fn batch_file_path(&self, file_id: i64) -> PathBuf {
+        self.path.join(BATCH_FILE_NAME_PREFIX.to_owned() + &format!(file_id_pattern!(), file_id))
+    }
+
+    fn increment_next_batch_id(&self, waiter: &mut Waiter<FileStorageSharedState>) -> io::Result<()> {
+        waiter.next_batch_id += 1;
+        waiter.batch_id_file.seek(SeekFrom::Start(0))?;
+        let batch_id_str = format!("{}", waiter.next_batch_id);
+        waiter.batch_id_file.write_all(batch_id_str.as_bytes())
+    }
 }
 
+impl BatchStorage<Box<BinaryBatch>> for FileStorage {
+    fn store<R>(&self, records: R, batch_factory: &impl BatchFactory<R>) -> Result<(), Error> {
+        let mut waiter = self.shared_state.lock();
+        if waiter.stopped {
+            return Err(Error::new(ErrorKind::Interrupted, format!("The storage {:?} has been shut down", self)))
+        }
+
+        let next_batch_id = waiter.next_batch_id;
+        let batch = batch_factory.create_batch(records, next_batch_id)?;
+
+        while self.is_capacity_exceeded(&batch, &waiter) {
+            waiter.wait()?;
+        }
+
+        let batch_file_path = self.batch_file_path(next_batch_id);
+        OpenOptions::new().write(true).create_new(true).open(&batch_file_path)?.write_all(&batch.bytes)?;
+
+        waiter.file_ids.push_back(next_batch_id);
+
+        waiter.notify_one();
+
+        Ok(())
+    }
+
+    fn get(&self) -> Result<Box<BinaryBatch>, Error> {
+        unimplemented!()
+    }
+
+    fn remove(&self) -> Result<(), Error> {
+        unimplemented!()
+    }
+
+    fn is_persistent(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn is_empty(&self) -> bool {
+        unimplemented!()
+    }
+
+    fn shutdown(self) {
+        unimplemented!()
+    }
+}
